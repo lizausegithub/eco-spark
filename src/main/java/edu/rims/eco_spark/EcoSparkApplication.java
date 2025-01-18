d886b9fdaa70e95f33812c9723f90f1671947070
lizausegithub <banditasnpt@gmail.com>
2025-01-18 12:25:49 +0530

    initial env setup

---
+++ src/main/java/edu/rims/eco_spark/EcoSparkApplication.java
@@ -0,0 +1,13 @@
+package edu.rims.eco_spark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoSparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoSparkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/rims/eco_spark/EcoSparkApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.rims.eco_spark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoSparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
