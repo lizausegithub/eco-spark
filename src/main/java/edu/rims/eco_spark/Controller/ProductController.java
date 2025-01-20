c86d69370b9c93ac343f9dd17a9cc16a59ed6341
lizausegithub <banditasnpt@gmail.com>
2025-01-20 23:52:47 +0530

    change home page

--- src/main/java/edu/rims/eco_spark/Controller/ProductController.java
+++ src/main/java/edu/rims/eco_spark/Controller/ProductController.java
@@ -1,10 +1,13 @@
 package edu.rims.eco_spark.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping("/Product")
 public class ProductController {
     @GetMapping({"/home","/"})
     String home(){
-        return "home";
+        return "Product/home";
     }
 }
