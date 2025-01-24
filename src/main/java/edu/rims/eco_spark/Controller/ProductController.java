268409fc4e74d1130dc96ec53ba40d243e27be2b
sushrita1parida <sushritaparida7@gmail.com>
2025-01-24 22:42:28 +0530

    add image

--- src/main/java/edu/rims/eco_spark/Controller/ProductController.java
+++ src/main/java/edu/rims/eco_spark/Controller/ProductController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/product")
+@RequestMapping("/Product")
 public class ProductController {
     @GetMapping({"/home","/"})
     String home(){
-        return "product/home";
+        return "Product/home";
     }
 }
