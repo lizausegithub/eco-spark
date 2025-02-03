c0ab5b2a95913c39f6990309a9abf5c4a49f0603
lizausegithub <banditasnpt@gmail.com>
2025-02-03 21:30:45 +0530

    edit homepage

---
+++ src/main/resources/static/script/nav.js
@@ -0,0 +1,9 @@
+// window.addEventListener('scroll', function () {
+//     const nav = this.document.querySelector('nav');
+
+//     if (window.scrollY >= nav.offsetTop) {
+//         nav.classList.add('sticky');
+//     } else {
+//         nav.classList.remove('sticky');
+//     }
+// })
