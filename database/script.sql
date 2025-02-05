9969dcedf65d63a72db8040ccded390e5a83db3e
lizausegithub <banditasnpt@gmail.com>
2025-02-05 09:47:27 +0530

    create snippet

--- database/script.sql
+++ database/script.sql
@@ -17,6 +17,7 @@ CREATE TABLE category (
 
 CREATE TABLE product (
     product_id VARCHAR(255) PRIMARY KEY,
+    category_id INT NULL,
     product_title VARCHAR(50) NOT NULL,
     product_description TEXT NULL,
     product_image_url VARCHAR NULL,
@@ -25,5 +26,45 @@ CREATE TABLE product (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     created_by INT, 
+    updated_by INT,
+    FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE CASCADE
+);
+
+CREATE TABLE user (
+    user_id VARCHAR(255) PRIMARY KEY,
+    user_name VARCHAR(255) NOT NULL,
+    user_email VARCHAR(255) UNIQUE NOT NULL,
+    user_password VARCHAR(255) NOT NULL,
+    user_address TEXT,
+    user_phone VARCHAR(20),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_by INT, 
     updated_by INT
+);
+
+CREATE TABLE order (
+    order_id VARCHAR(255) PRIMARY KEY,
+    user_id INT NOT NULL,
+    order_total_price DECIMAL(10,2) NOT NULL,
+    order_status ENUM('Pending', 'Processing', 'Shipped', 'Delivered' ,'Cancelled') DEFAULT 'pending',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_by INT, 
+    updated_by INT,
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+);
+
+CREATE TABLE order_item (
+    order_item_id VARCHAR(255) PRIMARY KEY,
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    order_item_quantity INT NOT NULL,
+    order_item_price DECIMAL(10,2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_by INT, 
+    updated_by INT,
+    FOREIGN KEY (order_id) REFERENCES order(id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE CASCADE
 );
