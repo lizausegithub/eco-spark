c1220077f0b005717f5335d519f560898734df8b
lizausegithub <banditasnpt@gmail.com>
2025-02-06 12:30:25 +0530

    rename file

--- src/main/java/edu/rims/eco_spark/Controller/ProductController.java
+++ src/main/java/edu/rims/eco_spark/Controller/ProductController.java
@@ -1,14 +1,5 @@
 package edu.rims.eco_spark.Controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/Product")
 public class ProductController {
-    @GetMapping({"/home","/"})
-    String home(){
-        return "Product/home";
-    }
+
 }
