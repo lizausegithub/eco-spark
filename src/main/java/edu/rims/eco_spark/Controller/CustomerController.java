410e4aa1709343748bdc495c2760dd1c16c3325c
lizausegithub <banditasnpt@gmail.com>
2025-02-10 12:08:38 +0530

    category add to fontend

--- src/main/java/edu/rims/eco_spark/Controller/CustomerController.java
+++ src/main/java/edu/rims/eco_spark/Controller/CustomerController.java
@@ -1,14 +1,27 @@
 package edu.rims.eco_spark.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import edu.rims.eco_spark.entity.Category;
+import edu.rims.eco_spark.repository.CategoryRepository;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @GetMapping({ "/home", "/" })
-    String home() {
+    String home(Model model) {
+        List<Category> categories = categoryRepository.findAll();
+
+        model.addAttribute("categories", categories);
         return "customer/home";
     }
 }
